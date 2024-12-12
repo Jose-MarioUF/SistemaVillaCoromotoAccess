65c44bb7b686518e04951f41739300084c0029a8
Jose-MarioUF <julloa60694@ufide.ac.cr>
2024-12-12 03:49:21 -0600

    Modulo de Historico de Facturas
    historico

---
+++ CoromotoAccess/CoromotoAccess/Controllers/FacturasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoromotoAccess.Controllers
+{
+    public class FacturasController : Controller
+    {
+        // GET: Facturas
+        public ActionResult Historico()
+        {
+            return View();
+        }
+    }
+}
