1dd99d030681fb32b3e981df29990994f6df8200
Alejrr2 <124023428+Alejrr2@users.noreply.github.com>
2024-12-11 18:51:43 -0600

    Reservas y Activos

---
+++ CoromotoAccess/CoromotoAccess/Controllers/ActivosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoromotoAccess.Controllers
+{
+    public class ActivosController : Controller
+    {
+        // GET: Activos
+        public ActionResult AdministrarActivos()
+        {
+            return View();
+        }
+    }
+}
---
+++ CoromotoAccess/CoromotoAccess/Controllers/ReservaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoromotoAccess.Controllers
+{
+    public class ReservaController : Controller
+    {
+        public ActionResult AdministrarReservas()
+        {
+            return View();
+        }
+    }
+}
